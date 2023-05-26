ff01172c8b5e952c5a408db4162424c40843a642
lbuckl <ya_mvn@mail.ru>
2023-05-27 00:26:10 +0500

    COREUI: Добавил модуль coreui, самописный роутер и утили

---
+++ coreui/src/main/java/com/molchanov/coreui/router/Router.kt
@@ -0,0 +1,86 @@
+package com.molchanov.coreui.router
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import javax.inject.Inject
+
+/**
+ * Класс навигации по фрагментам
+ * @fragmentRepId - id фрагмента или сслыка на контейнер
+ * @fragment - фрагмент для замены
+ * @tag - тэг под которым был сохранён фрагмент
+ */
+class Router @Inject constructor() : IRouter {
+
+    override fun addFragment(
+        fragmentManager: FragmentManager, fragmentRepId: Int,
+        fragment: Fragment, tag: String
+    ) {
+
+        val oldFragment = fragmentManager.findFragmentByTag(tag)
+
+        if (oldFragment == null) {
+            fragmentManager.beginTransaction()
+                .add(fragmentRepId, fragment, tag)
+                .commit()
+        } else {
+            fragmentManager.beginTransaction()
+                .replace(fragmentRepId, oldFragment, tag)
+                .commit()
+        }
+    }
+
+    override fun replaceFragment(
+        fragmentManager: FragmentManager, fragmentRepId: Int,
+        fragment: Fragment, tag: String
+    ) {
+
+        val oldFragment = fragmentManager.findFragmentByTag(tag)
+
+        if (oldFragment == null) {
+            fragmentManager.beginTransaction()
+                .replace(fragmentRepId, fragment, tag)
+                .commit()
+        } else {
+            fragmentManager.beginTransaction()
+                .replace(fragmentRepId, oldFragment, tag)
+                .commit()
+        }
+    }
+
+    override fun replaceFragmentWithMessage(
+        fragmentManager: FragmentManager, fragmentRepId: Int,
+        fragment: Fragment, tag: String,
+        message: Bundle
+    ) {
+
+        val oldFragment = fragmentManager.findFragmentByTag(tag)
+
+        try {
+            if (oldFragment == null) {
+                fragmentManager.beginTransaction()
+                    .add(fragmentRepId, fragment::class.java, message, tag)
+                    .commit()
+            } else {
+                fragmentManager.beginTransaction()
+                    .replace(fragmentRepId, oldFragment::class.java, message, tag)
+                    .commit()
+            }
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun deleteFragment(
+        fragmentManager: FragmentManager, fragmentRepId: Int,
+        fragment: Fragment, tag: String
+    ) {
+
+        fragmentManager.findFragmentByTag(tag)?.let {
+            fragmentManager.beginTransaction()
+                .remove(it)
+                .commit()
+        }
+    }
+}
---
+++ coreui/src/main/java/com/molchanov/coreui/utils/constants.kt
@@ -0,0 +1,7 @@
+package com.molchanov.coreui.utils
+
+//Задержка перед установкой нового AppState
+const val APP_STATE_DELAY = 100L
+
+//Задержка перед отображением или блокировкой прогрес бара
+const val PROGRESS_DELAY = 500L
